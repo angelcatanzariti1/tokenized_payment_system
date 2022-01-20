d5b7823632b842cabfb947725077e78564fe121f
Angel Catanzariti <angel@innship.com>
2022-01-20 14:35:22 -0300

    declarations

--- tickets_payment.sol
+++ tickets_payment.sol
@@ -5,5 +5,26 @@ import "./SafeMath.sol";
 import "./ERC20.sol";
 
 contract Tickets{
-    
+    //Token contract instance
+    ERC20Basic private token;
+
+    //Declarations
+    address payable public owner;
+
+    //Constructor
+    constructor() public{
+        token = new ERC20Basic(10000000);
+        owner = msg.sender;
+    }
+
+    //Customers
+    struct customer{
+        uint tokens_bought;
+        string[] attractions_visited;
+    }
+
+    //Customers registration
+    mapping(address => customer) public Customers;
+
+
 }
