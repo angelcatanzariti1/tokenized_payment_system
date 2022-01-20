b5662ceef32996ca8d90a4217241a1acfc7f11f5
Angel Catanzariti <angel@innship.com>
2022-01-20 15:12:38 -0300

    buy tokens

--- tickets_payment.sol
+++ tickets_payment.sol
@@ -6,6 +6,8 @@ import "./ERC20.sol";
 
 contract Tickets{
 
+    using SafeMath for uint;
+
     //------------------------------ INITIAL DECLARATIONS ----------------------------------------
     //Token contract instance
     ERC20Basic private token;
@@ -29,12 +31,33 @@ contract Tickets{
     mapping(address => customer) public Customers;
 
     //------------------------------ TOKENS MANAGEMENT -------------------------------------------
-    //Set token price
-    function TokenPrice(uint _numTokens) internal pure returns(uint){
+    //Get token price
+    function GetTokenPrice(uint _numTokens) internal pure returns(uint){
         //Convert tokens to eth
-        return _numTokens*(1 ether);
+        return _numTokens.mul(1 ether);
     }
 
+    //Buy tokens
+    function BuyTokens(uint _numTokens) public payable{
+        //get price
+        uint price = GetTokenPrice(_numTokens);
+        //check if the amount of eth in payment is enough
+        require(msg.value >= price, "Not enough ETH to buy that quantity of tokens");
+        //check change
+        uint returnValue = msg.value.sub(price);
+        msg.sender.transfer(returnValue);
+        //get available tokens
+        uint balance = balanceOf();
+        require(_numTokens <= balance, "Buy less tokens");
+        //transfer tokens to customer
+        token.transfer(msg.sender, _numTokens);
+        //store data
+        Customers[msg.sender].tokens_bought = _numTokens;
+    }
 
+    //Get amount of available tokens
+    function balanceOf() public view returns(uint){
+        return token.balanceOf(address(this));
+    }
 
 }
